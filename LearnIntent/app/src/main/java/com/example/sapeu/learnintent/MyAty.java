9cfe800be12bf303f1f0d56e2f0fcfa13aa4eb56
sapeu.lee@qq.com <sapeu.lee@qq.com>
2016-08-07 16:52:19 +0800

    学习显式Intent
    Signed-off-by: sapeu.lee@qq.com <sapeu.lee@qq.com>

---
+++ LearnIntent/app/src/main/java/com/example/sapeu/learnintent/MyAty.java
@@ -0,0 +1,15 @@
+package com.example.sapeu.learnintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sapeu on 2016/8/7.
+ */
+public class MyAty extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_aty);
+    }
+}
