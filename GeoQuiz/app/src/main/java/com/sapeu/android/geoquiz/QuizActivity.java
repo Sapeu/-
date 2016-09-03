1d13021cfa1db3971f467c742e606d38e2e8c3e7
Sapeu <707189200@qq.com>
2016-09-03 14:12:00 +0800

    Activity 的生命周期
    Signed-off-by: Sapeu <707189200@qq.com>

--- GeoQuiz/app/src/main/java/com/sapeu/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/sapeu/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.sapeu.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -33,7 +37,19 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) Called");
         setContentView(R.layout.activity_quiz);
+
+        /*Question question;
+        try{
+            question = mQuestionBank[9];
+        } catch (ArrayIndexOutOfBoundsException ex){
+            Log.e(TAG,"Index was out of bounds",ex);
+        }*/
+
+        if (null != savedInstanceState){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -57,7 +73,7 @@ public class QuizActivity extends AppCompatActivity {
         mPrevImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (Math.abs(mCurrentIndex - 1)) % mQuestionBank.length;
+                mCurrentIndex = (mQuestionBank.length - 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
@@ -81,6 +97,36 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -98,5 +144,12 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.d(TAG,"onSaveInstanceState(Bundle) called");
+        saveInstanceState.putInt(KEY_INDEX,mCurrentIndex);
     }
 }
