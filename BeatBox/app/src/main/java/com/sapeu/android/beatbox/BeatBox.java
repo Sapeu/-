9aa6fdd6260dbe246e3cda661ea6ac2dc385b104
Sapeu <707189200@qq.com>
2016-09-20 00:22:31 +0800

    学习使用SoundPool 播放音频
    Signed-off-by: Sapeu <707189200@qq.com>

--- BeatBox/app/src/main/java/com/sapeu/android/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/sapeu/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.sapeu.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -13,32 +16,61 @@ import java.util.List;
  */
 public class BeatBox {
     private static final String TAG = "BeatBox";
-    private static final String SOUNDS_FOLDER = "sample_sounds";
+//    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final String SOUNDS_FOLDER = "animal_sounds";
+    private static final int MAX_SOUNDS = 5;
+    ;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<Sound>();
+    private SoundPool mSoundPool;
 
-    public BeatBox(Context context){
+    public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
 
-    private void loadSounds(){
+    public void play(Sound sound){
+        Integer soundId = sound.getSoundId();
+        if (soundId == null){
+            return;
+        }
+        // 音频ID、左音量、右音量、优先级（无效）、是否循环（传入0值，代表不循环）、播放速率
+        mSoundPool.play(soundId,1.0f,1.0f,1,0,1.0f);
+    }
+
+    public void release(){
+        mSoundPool.release();
+    }
+
+    private void loadSounds() {
         String[] soundNames;
         try {
             soundNames = mAssets.list(SOUNDS_FOLDER);
-            Log.i(TAG,"Found " + soundNames.length + " sounds");
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
         } catch (IOException e) {
 //            e.printStackTrace();
-            Log.e(TAG,"Could not list assets",e);
+            Log.e(TAG, "Could not list assets", e);
             return;
         }
-
         for (String fileName : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + fileName;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + fileName;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+//            e.printStackTrace();
+                Log.e(TAG, "Could not load sound " + fileName,e);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
