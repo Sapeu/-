0886a066d578dde7a2a939461a595bbb0df7a73e
Sapeu <707189200@qq.com>
2016-09-23 00:18:14 +0800

    创建NerdLauncher应用
    Signed-off-by: Sapeu <707189200@qq.com>

---
+++ NerdLauncher/app/src/main/java/com/sapeu/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,50 @@
+package com.sapeu.android.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Sapeu on 2016/9/22.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+
+    private static final String TAG = "NerdLauncherFragment";
+
+    private RecyclerView mRecyclerView;
+
+    public static Fragment newInstance(){
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher,container,false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setAdapter();
+        return v;
+    }
+
+    private void setAdapter(){
+        Intent startUpIntent = new Intent(Intent.ACTION_MAIN);
+        startUpIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startUpIntent,0);
+        Log.i(TAG,"Found "+activities.size()+" activities.");
+    }
+}
