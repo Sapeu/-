05a40ba0c6947a23922a32f1dbd000b421c45fa8
Sapeu <707189200@qq.com>
2016-11-23 00:10:48 +0800

    学习属性动画 动画集
    Signed-off-by: Sapeu <707189200@qq.com>

--- Sunset/app/src/main/java/com/sapeu/android/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/sapeu/android/sunset/SunsetFragment.java
@@ -1,12 +1,16 @@
 package com.sapeu.android.sunset;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by Sapeu on 2016/11/22.
@@ -17,6 +21,10 @@ public class SunsetFragment extends Fragment {
     private View mSunsetView;
     private View mSunView;
     private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
 
     public static SunsetFragment newInstance() {
 
@@ -35,6 +43,11 @@ public class SunsetFragment extends Fragment {
         mSunView = mSunsetView.findViewById(R.id.sun);
         mSkyView = mSunsetView.findViewById(R.id.sky);
 
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
         mSunsetView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,10 +60,28 @@ public class SunsetFragment extends Fragment {
 
     private void startAnimation() {
         float sunYStart = mSunView.getTop();
-        float sunYEnd = mSunView.getHeight();
+        float sunYEnd = mSkyView.getHeight();
 
         ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView,"y",sunYStart,sunYEnd)
                 .setDuration(3000);
-        heightAnimator.start();
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView,"backgroundColor",mBlueSkyColor,mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        ObjectAnimator nightSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView,"backgroundColor",mSunsetSkyColor,mNightSkyColor)
+                .setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+//        heightAnimator.start();
+//        sunsetSkyAnimator.start();
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(heightAnimator)
+                .with(sunsetSkyAnimator)
+                .before(nightSkyAnimator);
+        animatorSet.start();
     }
 }
